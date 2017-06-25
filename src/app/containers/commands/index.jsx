5514d9301ce8416b82d7f7ea7a5fc879cbef9e8c
Andrew Mirsky <andrew@mirsky.net>
2017-06-24 20:02:30 -0700

    restructuring in order to reuse the 'timeline' list of events

--- src/app/containers/commands/index.jsx
+++ src/app/containers/commands/index.jsx
@@ -4,17 +4,7 @@ import { bindActionCreators } from "redux";
 import {requestSystemCommands} from "action_creators";
 
 import {Timeline, Row, Col, Button} from 'antd';
-import FontAwesome from 'react-fontawesome';
-
-import CustomScroll from 'react-custom-scroll';
-import './window.scss';
-import {Colors, CommandStatus} from "../../enums";
-
-const commandStyle = {
-    textAlign:'left',
-    fontFamily: 'monospace',
-    fontSize:16
-};
+import List from './list';
 
 const columnStyle = {
     padding: 10
@@ -40,23 +30,6 @@ class CommandWindow extends React.Component {
         this.props.getCommands();
     }
 
-    createDot = (status) => {
-        switch(status) {
-            case CommandStatus.COMPLETED:
-                return(<FontAwesome name='circle-o' size='2x' style={{color:Colors.darkBlue.color}}/>);
-            case CommandStatus.ACTIVE:
-                return(<FontAwesome name='circle-o-notch' size='2x' spin={true} style={{color:Colors.goldenRod.color}}/>);
-            case CommandStatus.ERROR:
-                return(<FontAwesome name='circle-o' size='2x' style={{color:Colors.paradisePink.color}}/>);
-            case CommandStatus.SKIPPED:
-                return(<FontAwesome name='circle-o' size='2x' style={{color:Colors.lightGray.color}}/>);
-            case CommandStatus.PENDING:
-            default:
-                return(<FontAwesome name='circle-o' size='2x' style={{color:'rgba(0,0,0,0.1)'}}/>);
-
-        }
-    };
-
     render() {
 
         return (
@@ -65,35 +38,13 @@ class CommandWindow extends React.Component {
                 <Col span={12} style={columnStyle}>
                     <h3>Commands</h3>
                     <div style={windowStyle}>
-                        <CustomScroll heightRelativeToParent="100%">
-                            <Timeline>
-                                {
-                                    this.props.commands.map((command, idx) => {
-                                        return(<Timeline.Item dot={this.createDot(command.status)} style={commandStyle} key={idx}>{command.command}</Timeline.Item>)
-                                    })
-                                }
-                            </Timeline>
-                        </CustomScroll>
-                        <div>
-                            <Button>
-                                Run
-                                <FontAwesome name='play'/>
-                            </Button>
-                        </div>
+                        <List commands={this.props.commands}/>
                     </div>
                 </Col>
                 <Col span={12} style={columnStyle}>
                     <h3>Machine</h3>
                     <div style={windowStyle}>
-                    <CustomScroll heightRelativeToParent="100%">
-                        <Timeline>
-                            {
-                                this.props.commands.map((command, idx) => {
-                                    return(<Timeline.Item dot={this.createDot(command.status)} style={commandStyle} key={idx}>{command.command}</Timeline.Item>)
-                                })
-                            }
-                        </Timeline>
-                    </CustomScroll>
+                        <List commands={this.props.commands}/>
                     </div>
                 </Col>
             </Row>
--- src/app/containers/commands/list.jsx
+++ src/app/containers/commands/list.jsx
@@ -2,15 +2,55 @@ import React from 'react';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
+import FontAwesome from 'react-fontawesome';
+import CustomScroll from 'react-custom-scroll';
+import {Timeline} from 'antd';
+
+import {Colors, CommandStatus} from "enums";
+import './list.scss';
+
+
+const commandStyle = {
+    textAlign:'left',
+    fontFamily: 'monospace',
+    fontSize:16
+};
+
 class List extends React.Component {
     constructor(props) {
         super(props);
         this.state = { };
     }
 
+    createDot = (status) => {
+        switch(status) {
+            case CommandStatus.COMPLETED:
+                return(<FontAwesome name='circle-o' size='2x' style={{color:Colors.darkBlue.color}}/>);
+            case CommandStatus.ACTIVE:
+                return(<FontAwesome name='circle-o-notch' size='2x' spin={true} style={{color:Colors.goldenRod.color}}/>);
+            case CommandStatus.ERROR:
+                return(<FontAwesome name='circle-o' size='2x' style={{color:Colors.paradisePink.color}}/>);
+            case CommandStatus.SKIPPED:
+                return(<FontAwesome name='circle-o' size='2x' style={{color:Colors.lightGray.color}}/>);
+            case CommandStatus.PENDING:
+            default:
+                return(<FontAwesome name='circle-o' size='2x' style={{color:'rgba(0,0,0,0.1)'}}/>);
+
+        }
+    };
+
     render() {
         return (
-        <div></div>
+            <CustomScroll heightRelativeToParent="100%">
+                <Timeline>
+                {
+                    this.props.commands.map((command, idx) => {
+                        return(<Timeline.Item dot={this.createDot(command.status)} style={commandStyle} key={idx}>{command.command}</Timeline.Item>)
+                    })
+                }
+                </Timeline>
+            </CustomScroll>
+
         )
     }
 }
